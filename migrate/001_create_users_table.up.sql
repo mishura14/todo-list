da3678b35a410bbb9af67e3111df9ddb2187a239
mishura <misuraaleksej60@gmail.com>
2025-11-27 16:55:33 +0300

    Исправление sql миграций

--- migrate/001_create_users_table.up.sql
+++ migrate/001_create_users_table.up.sql
@@ -2,8 +2,10 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
+    phone VARCHAR(20),
     password_hash VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX idx_users_email ON users(email);
