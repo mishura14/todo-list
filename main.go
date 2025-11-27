5d2159701ccaf0fb2dde4cb20ad1274a9f8d5e56
mishura <misuraaleksej60@gmail.com>
2025-11-27 15:30:14 +0300

    Добавление redis

--- main.go
+++ main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"fmt"
-	"git-register-project/internal/db"
+	"git-register-project/internal/Database/postgres"
+	"git-register-project/internal/Database/redis"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	db, err := db.ConnectDB()
+	//подключение  postgresql
+	db, err := postgres.ConnectDB()
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 	fmt.Println("Database connection established")
+	//подключение redis
+	rdb, err := redis.ConnectRedis()
+	if err != nil {
+		panic(err)
+	}
+	defer rdb.Close()
+	fmt.Println("Connection Redis")
 
 	r := gin.Default()
 
